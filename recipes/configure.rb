64cfda8d8a0650ddeaea4b12509c855ad6ac8cb5
Franklin Wise <franklin@krave-n.com>
2014-11-09 20:57:25 -0800

    removed opsworks_hosts

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -4,8 +4,6 @@
 
 include_recipe 'runit'
 
-include_recipe 'haproxy::opsworks_hosts'
-
 runit_service "haproxy" do
   supports :restart => true, :status => true
   action :nothing # only define so that it can be restarted if the config changed
@@ -18,7 +16,6 @@ template "#{node['haproxy']['conf_dir']}/haproxy.cfg" do
   group "root"
   mode 0644
   variables(
-     :nginx_hosts => node[:haproxy_custom][:nginx_hosts]
   )
   notifies :restart, "runit_service[haproxy]"
 end
