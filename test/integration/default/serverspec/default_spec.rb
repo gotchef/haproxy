4573093ee6bdfe2e60909476c1e3056f06884834
Franklin Wise <franklin@krave-n.com>
2015-02-08 11:46:00 -0800

    updated spec

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,10 +2,9 @@
 require 'serverspec'
 
 # Required by serverspec
-set :backend, :exec
+#set :backend, :exec
 
 describe service('haproxy') do
-  it { should be_enabled }
   it { should be_running }
 end
 
